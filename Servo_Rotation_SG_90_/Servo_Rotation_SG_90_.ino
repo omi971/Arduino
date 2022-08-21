729c921c89b6dffd563f9cf7e3bd17a37333324f
omi971 <nazmul.haque.omi@g.bracu.ac.bd>
2022-08-22 01:33:07 +0600

    Muscle Sensor controlling Servo

--- Servo_Rotation_SG_90_/Servo_Rotation_SG_90_.ino
+++ Servo_Rotation_SG_90_/Servo_Rotation_SG_90_.ino
@@ -1,37 +1,37 @@
 #include <Servo.h>
 Servo servo;
 
-//int angle = 10;
-int d = 1000;
+int angle = 10;
+int d = 10;
 int input;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Please type the angle: ");
-  servo.attach(8);
+  servo.attach(9);
   servo.write(0);
 }
 
 void loop()
 { 
-  while (Serial.available()){
-    input = Serial.parseInt();
-//    Serial.print("You Typed: ");
-//    Serial.println("Servo Serial: ");
-    Serial.println(input);
-    servo.write(input);
-    delay(1000);
-    }
-// // scan from 0 to 180 degrees
-//  for(angle = 10; angle < 180; angle++)  
-//  {                                  
-//    servo.write(angle);               
-//    delay(d);                   
-//  } 
-//  // now scan back from 180 to 0 degrees
-//  for(angle = 180; angle > 10; angle--)    
-//  {                                
-//    servo.write(angle);           
-//    delay(d);       
-//  } 
+//  while (Serial.available()){
+//    input = Serial.parseInt();
+////    Serial.print("You Typed: ");
+////    Serial.println("Servo Serial: ");
+//    Serial.println(input);
+//    servo.write(input);
+//    delay(1000);
+//    }
+ // scan from 0 to 180 degrees
+  for(angle = 10; angle < 180; angle++)  
+  {                                  
+    servo.write(angle);               
+    delay(d);                   
+  } 
+  // now scan back from 180 to 0 degrees
+  for(angle = 180; angle > 10; angle--)    
+  {                                
+    servo.write(angle);           
+    delay(d);       
+  } 
 } 
